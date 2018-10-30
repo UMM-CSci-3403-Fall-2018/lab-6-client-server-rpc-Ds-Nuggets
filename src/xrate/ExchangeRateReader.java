8b56ff47dd207289e20b3757748d97ceaa3915bb
bella148 <bella148@morris.umn.edu>
2018-10-30 11:23:57 -0500

    Finish lab. Commented everything up nicely.

--- src/xrate/ExchangeRateReader.java
+++ src/xrate/ExchangeRateReader.java
@@ -1,23 +1,24 @@
 package xrate;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.io.*;
+import java.util.Properties;
 
 /**
  * Provide access to basic currency exchange rate services.
  * 
- * @author PUT YOUR TEAM NAME HERE
+ * @author Blake Bellamy, Prince Nwaonicha
  */
 public class ExchangeRateReader
 {
+
+    //Construct 'global' variables to be accessed throughout the program.
+    public URL globalURL;
+    private String accessKey;
 
     /**
      * Construct an exchange rate reader using the given base URL. All requests
@@ -30,10 +31,7 @@ public class ExchangeRateReader
      * @param baseURL
      *            the base URL for requests
      */
-
-    URL globalURL;
-
-    public ExchangeRateReader(String baseURL) throws MalformedURLException
+    public ExchangeRateReader(String baseURL) throws MalformedURLException, IOException
     {
         // TODO Your code here
         /*
@@ -42,8 +40,12 @@ public class ExchangeRateReader
          * the two methods below. All you need to do here is store the
          * provided `baseURL` in a field so it will be accessible later.
          */
-        //"facultypages.morris.umn.edu"
+
+        //Assigns the global variable 'globalURL' the base string that the user constructs
         globalURL = new URL(baseURL);
+
+        //Read's access key
+        readAccessKeys();
 
     }
 
@@ -64,18 +66,20 @@ public class ExchangeRateReader
      */
     public float getExchangeRate(String currencyCode, int year, int month, int day) throws IOException, UnsupportedOperationException
     {
-        // TODO Your code here
+        // Sets the initialized globalUrl to the format of the url website that we are using with all parameters.
+        globalURL = new URL(globalURL.toString() + year + "-" + safeDate(month) + "-" + safeDate(day) + "?access=" + accessKey);
 
-        globalURL = new URL(globalURL.toString() + "-" + year + "-" + safeDate(month) + "-" + safeDate(day) + "?access=");
-
+        // Begin 'accepting' the raw text from the website.
         InputStream inputStream = globalURL.openStream();
 
+        // Begin reading that raw text to an object.
         Reader readme = new InputStreamReader(inputStream);
 
+        // We get the raw text of the website and we turn it into a formatted Json that we can navigate to.
         JsonObject urlInfo = new JsonParser().parse(readme).getAsJsonObject();
 
-        //Return dummy object
-        return 0;
+        //Call getRate to return the info that we want.
+        return getRate(urlInfo, currencyCode);
 
     }
 
@@ -98,33 +102,81 @@ public class ExchangeRateReader
      */
     public float getExchangeRate(String fromCurrency, String toCurrency, int year, int month, int day) throws IOException, UnsupportedOperationException
     {
-        // TODO Your code here
+        // Sets the initialized globalUrl to the format of the url website that we are using with all parameters.
+        globalURL = new URL(globalURL.toString() + year + "-" + safeDate(month) + "-" + safeDate(day) + "?access_key=" + accessKey);
+
+        // Begin 'accepting' the raw text from the website.
+        InputStream inputStream = globalURL.openStream();
+
+        // Begin reading that raw text to an object.
+        Reader readme = new InputStreamReader(inputStream);
+
+        // We get the raw text of the website and we turn it into a formatted Json that we can navigate to.
+        JsonObject urlInfo = new JsonParser().parse(readme).getAsJsonObject();
+
+        //Call getRate to return the info that we want, but this time we divide the two different currencies to get the exchange rate 'difference'.
+        return getRate(urlInfo, fromCurrency) / getRate(urlInfo, toCurrency);
 
     }
+
+    /**
+     * Get the exchange rate of the first specified currency against the second
+     * on the specified date.
+     *
+     * @param ratesInfo
+     *              The object containing every element from the json (the api's response in this case)
+     * @param country_code
+     *              The 3 character currency code associated with each country
+     * @return the desired exchange rate
+     *
+     */
 
     public float getRate(JsonObject ratesInfo, String country_code)
     {
 
-        JsonObject crates = ratesInfo.getAsJsonObject("rates");
-
+        //Get the json object called "rates", then get that new object's element called "country_code" as a float then return it.
         return ratesInfo.getAsJsonObject("rates").get(country_code).getAsFloat();
-
-        //return crates.get(country_code).getAsFloat();
-
-
 
     }
 
+    //This is so that we can return the single digit integers in the form of 0Int.
     public String safeDate(int num)
     {
+        //If it's less than 10 it's a single digit.
         if(num < 10)
         {
+            //Add a zero and return as a string.
             return "0" + Integer.toString(num);
         }
 
         else
         {
+            //It's not a single digit, so we are safe to return as a string.
             return Integer.toString(num);
         }
+    }
+
+    private void readAccessKeys() throws IOException {
+        Properties properties = new Properties();
+        FileInputStream in = null;
+        try {
+            // Don't change this filename unless you know what you're doing.
+            // It's crucial that we don't commit the file that contains the
+            // (private) access keys. This file is listed in `.gitignore` so
+            // it's safe to put keys there as we won't accidentally commit them.
+            in = new FileInputStream("etc/access_keys.properties");
+        } catch (FileNotFoundException e) {
+            /*
+             * If this error gets generated, make sure that you have the desired
+             * properties file in your project's `etc` directory. You may need
+             * to rename the file ending in `.sample` by removing that suffix.
+             */
+            System.err.println("Couldn't open etc/access_keys.properties; have you renamed the sample file?");
+            throw(e);
+        }
+        properties.load(in);
+        // This assumes we're using Fixer.io and that the desired access key is
+        // in the properties file in the key labelled `fixer_io`.
+        accessKey = properties.getProperty("fixer_io");
     }
 }
